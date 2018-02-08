a98c6c6d9b4a480ecc6a504ad5cc98d012ec0571
mao xiaoyong <453315655@qq.com>
2018-02-08 17:10:01 +0800

    2018-02-08 pm5

--- web practice/app.py
+++ web practice/app.py
@@ -1,9 +1,10 @@
-#coding:utf-8
+# coding:utf-8
 
-from flask import  Flask, request, render_template,redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 import db
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -13,21 +14,32 @@ def home():
 # def signin_form():
 #     return render_template('form.html')
 
+
 @app.route('/login', methods=['POST'])
 def login():
     loginid = request.form['loginid']
     password = request.form['password']
-    success,name = db.authenticate(loginid,password)
+    success, name = db.authenticate(loginid, password)
     # print(success,name)
     if success:
-        return redirect(url_for('book',user_name=name))
+        return redirect(url_for('book', user_name=name))
         # return render_template('index.html', username=name)
     return render_template('login.html', message='用户名或密码错误', loginid=loginid)
-    
-@app.route('/book/<user_name>',methods = ['GET'])
+
+
+@app.route('/book/<user_name>', methods=['GET'])
 def book(user_name):
+    searchword = request.args.get('ISBN')
     books = db.get_all_books()
-    return render_template('index.html',user_name= user_name,books = books)
+    books_ISBN_name = [book[0:2] for book in books]
+    one_book = None
+    if searchword != None:
+        for book in books:
+            if searchword == book[0]:
+                one_book = book
+                break
+    return render_template('index.html', user_name=user_name, books=books_ISBN_name, one_book=one_book)
+
 
 if __name__ == '__main__':
     # print(db.get_all_books())
